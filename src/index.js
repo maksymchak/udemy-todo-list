78187308bef5cfc68cfdcd4e82189e21d1d211c7
maksymchak <maksym4ak@gmail.com>
2019-03-12 11:35:58 +0200

    add main app component

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const el = (
+  <div>
+    <h1>My Todo List</h1>
+    <input placeholder="search"/>
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+    </ul>
+  </div>
+);
+
+ReactDom.render(el,
+  document.getElementById('root'));
