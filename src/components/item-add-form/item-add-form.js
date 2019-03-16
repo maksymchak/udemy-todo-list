36ff838d368795260cf01ef969d6b0c252d53ad6
maksymchak <maksym4ak@gmail.com>
2019-03-16 23:00:22 +0200

    use controled-component form

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -17,7 +17,10 @@ export default class ItemAddForm extends Component {
   onSubmit = (e) => {
     e.preventDefault();
     this.props.onItemAdded(this.state.label);
-  }
+    this.setState({
+      label: ''  
+    });
+  };
 
   render() {
 
@@ -28,7 +31,8 @@ export default class ItemAddForm extends Component {
         <input type="text"
                 className="form-control"
                 onChange={this.onLabelChange}
-                placeholder="What needs to be done" />
+                placeholder="What needs to be done" 
+                value={this.state.label} />
         <button 
           className="btn btn-outline-secondary">
           Add item
