c14b07652446db237ab73fc7a22d85cb2e0f4300
maksymchak <maksym4ak@gmail.com>
2019-03-16 14:36:20 +0200

    fix

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,11 +14,20 @@ export default class App extends Component {
 
   state = {
     todoData: [
-      { label: 'Drink Coffee', important: false, id: 1 },
-      { label: 'Make Awesome App', important: true, id: 2 },
-      { label: 'Have a lunch', important: false, id: 3 }
+      this.createTodoItem('Drink Coffee'),
+      this.createTodoItem('Make Awesome App'),
+      this.createTodoItem('Have a lunch')
     ]
   };
+
+  createTodoItem(label) {
+    return {
+      label,
+      important: false,
+      done: false,
+      id: this.maxId++
+    }
+  }
 
   deleteItem = (id) => {
     this.setState(( { todoData } ) => {
@@ -35,13 +44,7 @@ export default class App extends Component {
   }; 
 
   addItem = (text) => {
-
-    const newItem = {
-      label: text,
-      important: false,
-      id: this.maxId++
-    };
-
+    const newItem = this.createTodoItem(text);
     this.setState(({ todoData }) => {
 
       const newArr = [
