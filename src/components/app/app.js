109da5bc5cf185db7613920b643397738e87a760
maksymchak <maksym4ak@gmail.com>
2019-03-17 00:16:06 +0200

    search filter fix

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -99,7 +99,7 @@ export default class App extends Component {
     };
 
     return items.filter((item) => {
-      return item.label.indexOf(term) > -1;
+      return item.label.toLowerCase().indexOf(term.toLowerCase()) > -1;
     });
   }
 
