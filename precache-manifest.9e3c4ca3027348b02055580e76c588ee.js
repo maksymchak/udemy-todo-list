7c0a2ec10beae2acc7703e5dac0f78a63f5f0e84
maksymchak <maksym4ak@gmail.com>
2019-05-21 12:06:49 +0300

    Updates

---
+++ precache-manifest.9e3c4ca3027348b02055580e76c588ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e119c7bef1b0920aec2",
+    "url": "/udemy-todo-list/static/css/main.cc257bcd.chunk.css"
+  },
+  {
+    "revision": "2e119c7bef1b0920aec2",
+    "url": "/udemy-todo-list/static/js/main.02e06209.chunk.js"
+  },
+  {
+    "revision": "5f0abdc9ad9b84f3cf0d",
+    "url": "/udemy-todo-list/static/js/runtime~main.a4fa1fd7.js"
+  },
+  {
+    "revision": "4b6e8cb94b4e5c79626e",
+    "url": "/udemy-todo-list/static/js/2.810d0f62.chunk.js"
+  },
+  {
+    "revision": "1aa4db8be70a0e6bd26cbd6180585ab9",
+    "url": "/udemy-todo-list/index.html"
+  }
+];
